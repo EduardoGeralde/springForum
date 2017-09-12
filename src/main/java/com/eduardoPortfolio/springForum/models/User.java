6a7e6fd628f72fae982ab74324a1a6e44372f92b
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-12 12:39:09 -0300

    attributes added

--- src/main/java/com/eduardoPortfolio/springForum/models/User.java
+++ src/main/java/com/eduardoPortfolio/springForum/models/User.java
@@ -3,9 +3,41 @@ package com.eduardoPortfolio.springForum.models;
 @Entity
 public class User {
 	
-
 	private Long id;
+	private String name;
+	private String email;
 	private String login;
 	private String password;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
