15a56c081d31ca95cb9bb16b4c39d5e7e231cbcd
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-13 21:26:32 -0300

    action user topics created

--- src/main/java/com/eduardoPortfolio/springForum/controllers/UserController.java
+++ src/main/java/com/eduardoPortfolio/springForum/controllers/UserController.java
@@ -1,7 +1,10 @@
 package com.eduardoPortfolio.springForum.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,6 +24,13 @@ public class UserController {
 		ModelAndView mav = new ModelAndView("user/show");
 		mav.getModel().put("user", user);
 		return mav;
+	}
+	
+	@RequestMapping("/user/posts/{login}")
+	public String userTopics(@PathVariable("login") String login, Map<String,Object>model){
+		
+		model.put("topics", getDAOTopics.getUserTopics(getUserDAO().getUser(login)));
+		return "user/posts";
 	}
 	
 	
