f5a3f18967b388aebb82d236e976a58f1b2962f6
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-09-13 21:25:48 -0300

    process avatar created

--- src/main/java/com/eduardoPortfolio/springForum/controllers/HomeController.java
+++ src/main/java/com/eduardoPortfolio/springForum/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package com.eduardoPortfolio.springForum.controllers;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,6 +14,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.eduardoPortfolio.springForum.models.User;
 
@@ -39,16 +44,38 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value="/executeRegistry", method=RequestMethod.POST)
-	public String executeRegistry (@Valid User user, BindingResult bindingResult, 
-																HttpSession session){
+	public String executeRegistry (@Valid User user, 
+									BindingResult bindingResult, 
+									HttpSession session,
+						@RequestParam(value="avatar", required=false) MultipartFile avatar){
 		if (bindingResult.hasErrors()){
 			Map<String, Object> model = new HashMap <String, Object>();
 			model.put("user", user);
 			return register(model);
 		}
 		getDAOUser().persist(user);
+		
+		if (!avatar.isEmpty()){
+			processAvatar(user,avatar);
+		}
+		
 		session.setAttribute("user", user);
 		return "redirect:/";
+	}
+	
+	private void processAvatar(User user, MultipartFile avatar) {
+		File diretorio = new File("/springForum/avatares");
+		if (! diretorio.exists()) {
+			diretorio.mkdirs();
+		}
+			try {
+				FileOutputStream file = new FileOutputStream(diretorio.getAbsolutePath() + "/" + user.getLogin() + ".png");
+				file.write(avatar.getBytes());
+				file.close();
+			} catch (IOException ex) {
+				
+			}
+		
 	}
 	
 
